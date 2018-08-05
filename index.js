ff4a71c8ae223dfd0279a5a4ce7e6229dab36788
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-05 22:32:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo (name) {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayto (name, holiday) {
+function happyHolidayto(name, holiday) {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown (holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
