1528ae70ded0f708fa45237c3827e733d57626a8
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-05 22:30:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayto (holiday, name) {
+function happyHolidayto ("holiday", "name") {
   return "Happy ${holiday}, ${name}!"
 }
 
